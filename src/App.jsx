cbb1f222c7f9403754ad30e6e71baef4eec4917a
unknown <faisaaditya07@gmail.com>
2024-07-02 22:42:50 +0700

    FirstReact

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import HeroSection from "./components/Hero";
+import NaviBar from "./components/NaviBar";
+const App = () => {
+  return (
+    <div>
+      <NaviBar />
+      <HeroSection />
+    </div>
+  );
+};
+
+export default App;
